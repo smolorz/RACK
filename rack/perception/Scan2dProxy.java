66554d6598523db8a64765603851578bc7d33e43
Oliver Wulf <wulf@rts.uni-hannover.de>
2007-08-07 08:17:06 +0000

    remove debug println() and format the code

--- rack/perception/Scan2dProxy.java
+++ rack/perception/Scan2dProxy.java
@@ -25,79 +25,85 @@ import rack.main.tims.*;
 
 public class Scan2dProxy extends RackDataProxy
 {
-  public Scan2dProxy(int id, TimsMbx replyMbx)
-  {
-    super(RackName.create(RackName.SCAN2D, id), replyMbx, 2500);
-  }
-
-  public synchronized Scan2dDataMsg getData(int recordingTime)
-  {
-    try {
-      TimsRawMsg raw = getRawData(recordingTime);
-
-      if (raw != null) {
-        Scan2dDataMsg data = new Scan2dDataMsg(raw);
-        return(data);
-      } else {
-        return(null);
-      }
-    } catch(TimsException e) {
-      System.out.println(e.toString());
-      return(null);
+    public Scan2dProxy(int id, TimsMbx replyMbx)
+    {
+        super(RackName.create(RackName.SCAN2D, id), replyMbx, 2500);
     }
-  }
 
-  public synchronized Scan2dDataMsg getData()
-  {
-      long timeA, timeB;
-      
-    try {
-        timeA = System.currentTimeMillis();
-//      TimsRawMsg raw = getNextData();
-      TimsRawMsg raw = getRawData(0);
-      timeB = System.currentTimeMillis();
-      System.out.println("get data " + (timeB - timeA) + "ms");
+    public synchronized Scan2dDataMsg getData(int recordingTime)
+    {
+        try
+        {
+            TimsRawMsg raw = getRawData(recordingTime);
 
-      if (raw != null) {
-        Scan2dDataMsg data = new Scan2dDataMsg(raw);
-        return(data);
-      } else {
-        return(null);
-      }
-    } catch(TimsException e) {
-      System.out.println(e.toString());
-      return(null);
+            if (raw != null)
+            {
+                Scan2dDataMsg data = new Scan2dDataMsg(raw);
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (TimsException e)
+        {
+            System.out.println(e.toString());
+            return null;
+        }
     }
-  }
 
-  public int storeDataToFile(String filename, Scan2dDataMsg scan2dData)
-  {
-      if(scan2dData != null)
-      {
-         try
-          {
-	          PrintWriter fileOut = new PrintWriter(new BufferedWriter(
-	                  new FileWriter(filename)));
-	
-	          for (int i = 0; i < scan2dData.pointNum; i++)
-	          {
-	              fileOut.println(scan2dData.point[i].toString());
-	          }
-	          fileOut.flush();
-	          fileOut.close();
-          }
-         catch (IOException e)
-	      {
-	          System.out.println("Can't write 2d data to file.");
-	          System.out.println(e.toString());
-	      }
-      }
-	  return 0;
-  }
-  
-  public int storeDataToFile(String filename)
-  {
-  	Scan2dDataMsg scan2dData = getData(0);
-  	return storeDataToFile(filename, scan2dData);
-  }
+    public synchronized Scan2dDataMsg getData()
+    {
+        try
+        {
+            TimsRawMsg raw = getNextData();
+            // TimsRawMsg raw = getRawData(0);
+
+            if (raw != null)
+            {
+                Scan2dDataMsg data = new Scan2dDataMsg(raw);
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (TimsException e)
+        {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    public int storeDataToFile(String filename, Scan2dDataMsg scan2dData)
+    {
+        if (scan2dData != null)
+        {
+            try
+            {
+                PrintWriter fileOut = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+
+                for (int i = 0; i < scan2dData.pointNum; i++)
+                {
+                    fileOut.println(scan2dData.point[i].toString());
+                }
+                fileOut.flush();
+                fileOut.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Can't write 2d data to file.");
+                System.out.println(e.toString());
+            }
+        }
+        return 0;
+    }
+
+    public int storeDataToFile(String filename)
+    {
+        Scan2dDataMsg scan2dData = getData(0);
+        return storeDataToFile(filename, scan2dData);
+    }
 }
