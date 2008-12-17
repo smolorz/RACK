52e232901be801e0227c7dc9bf33ad664fbf0885
Volker Viereck <viereck>
2008-12-17 11:15:53 +0000

    BUGFIX: MSG_HOLD -> REVERT command

--- rack/navigation/pilot_proxy.cpp
+++ rack/navigation/pilot_proxy.cpp
@@ -48,6 +48,6 @@ int PilotProxy::holdCommand(pilot_hold_data *recv_data, ssize_t recv_datalen,
 int PilotProxy::revert(pilot_revert_data *recv_data, ssize_t recv_datalen,
                               uint64_t reply_timeout_ns)
 {
-    return proxySendDataCmd(MSG_PILOT_HOLD_COMMAND, recv_data, recv_datalen,
+    return proxySendDataCmd(MSG_PILOT_REVERT_COMMAND, recv_data, recv_datalen,
                              reply_timeout_ns);
 }
