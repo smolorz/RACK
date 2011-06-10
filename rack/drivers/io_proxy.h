efb3eecca75beff7d3d7aa970742e2e6b3accc4e
Matthias Hentschel <hentschel@rts.uni-hannover.de>
2011-06-10 15:50:01 +0200

    reduced max byte num for Velodyne

--- rack/drivers/io_proxy.h
+++ rack/drivers/io_proxy.h
@@ -27,7 +27,7 @@
 //# Io data defines
 //######################################################################
 #if defined (__MSG_VELODYNE__)
-#define IO_BYTE_NUM_MAX 840000              /**< maximum number of byte values */
+#define IO_BYTE_NUM_MAX 800000              /**< maximum number of byte values */
 #else // __MSG_SCANDRIVE__
 #define IO_BYTE_NUM_MAX 32 * 4              /**< maximum number of byte values */
 #endif
